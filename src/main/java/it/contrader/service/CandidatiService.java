8597ac159f5df3c90f69264522b417c9700aa118
V3nom89 <76600609+V3nom89@users.noreply.github.com>
2021-03-04 12:20:40 +0100

    Add files via upload

---
+++ src/main/java/it/contrader/service/CandidatiService.java
@@ -0,0 +1,21 @@
+package it.contrader.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.contrader.converter.CandidatiConverter;
+import it.contrader.dao.CandidatiRepository;
+import it.contrader.dto.CandidatiDTO;
+
+import it.contrader.model.Candidati;
+
+public class CandidatiService extends AbstractService<Candidati, CandidatiDTO> {
+	
+	@Autowired
+	private CandidatiConverter converter;
+	@Autowired
+	private CandidatiRepository repository;
+
+	public CandidatiDTO findByidCandidati(long id) {
+		return converter.toDTO(repository.findByidCandidati(id));
+	}
+}
